680a924f72e7ed87df76b7381e4ab039d47deb93
Josh Silverman <joshs.silverman@gmail.com>
2013-04-05 18:06:09 -0400

    scorecards now take into account new formatting method

--- app/controllers/scorecards_controller.rb
+++ app/controllers/scorecards_controller.rb
@@ -22,28 +22,19 @@ class ScorecardsController < ApplicationController
       group = card.groups.first
       next if group.nil? or group.question_format.nil? or group.answer_format.nil? or !card.publish
       next if card.scorecards.length > 0
-      question = {:card_id => card.id, :handle_id => card.deck.handle.id}
+      question = {
+        card_id: card.id, 
+        handle_id: card.deck.handle.id,
+        text: card.question_formatted,
+        answer: card.answer_formatted}
 
-      question_parts = group.question_format.split /\#{|}/
-      front = (question_parts[1] == "front") ? true : false
-      question['text'] = ""
-      question_parts.each_with_index do |part, i|
-        if i == 1
-          question['text'] += card[:front] if front
-          question['text'] += card[:back] if !front
-        else
-          question['text'] += part
-        end
-
-        question['answer'] = group.answer_format.gsub('#{front}', card.front).gsub('#{back}', card.back)
-        question['incorrect'] = []
-        i = 0
-        group.cards.shuffle.each do |other_card|
-          next if other_card.id == card.id
-          i += 1
-          question['incorrect'] << group.answer_format.gsub('#{front}', other_card.front).gsub('#{back}', other_card.back)
-          break if i == 3
-        end
+      question[:incorrect] = []
+      i = 0
+      group.cards.shuffle.each do |other_card|
+        next if other_card.id == card.id
+        i += 1
+        question[:incorrect] << other_card.answer_formatted
+        break if i == 3
       end
 
       @questions << question
